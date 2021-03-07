5a4e2f237acee5084dab77b41ff36e5bbfdcc389
koflgl <768472457@qq.com>
2021-03-07 20:14:14 +0800

    [add] 组合模式

---
+++ src/main/java/com/lgl/design/pattern/structural/composite/Course.java
@@ -0,0 +1,32 @@
+package com.lgl.design.pattern.structural.composite;
+
+/**
+ * @author lgl
+ * @Description
+ * @Date 2021/3/7 18:10
+ */
+public class Course extends CatalogComponent {
+    private String name;
+    private double price;
+
+    public Course(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String getName(CatalogComponent catalogComponent) {
+        return this.name;
+    }
+
+    @Override
+    public double getPrice(CatalogComponent catalogComponent) {
+        return this.price;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Course Name:"+name+" Price:"+price);
+    }
+
+}
